02adc66bfc6db1a84a9a98f9e80f89c1e2e8fb93
Joshua Goddard <joshua.goddard@students.mq.edu.au>
2016-10-25 11:00:03 +1100

    Changed the login_required_config decorator so that it passes the tests

--- dictionary/views.py
+++ dictionary/views.py
@@ -2,21 +2,51 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.shortcuts import render
 
-def login_required_config(f):
+from dictionary.forms import UserSignSearchForm
+
+
+def login_required_config(function):
     '''
     Like @login_required if the ALWAYS_REQUIRE_LOGIN setting is True.
     '''
-    if settings.ALWAYS_REQUIRE_LOGIN:
-        return login_required(f)
-    else:
-        return f
-        
+    def wrapper(*args, **kwargs):
+        if settings.ALWAYS_REQUIRE_LOGIN:
+            nonlocal function
+            function = login_required(function)
+            return function(*args, **kwargs)
+        else:
+            return function(*args, **kwargs)
+    return wrapper
+   
         
 @login_required_config
 def search(request):
-    if request.method == 'POST':
-        pass
+    form = UserSignSearchForm(request.GET)
+    if form.is_valid():
+        # need to transcode the query to our encoding
+        term = form.cleaned_data['query']
+        category = form.cleaned_data['category']
+        # safe search for non-authenticated users if the setting says so
+        safe = (not request.user.is_authenticated()) and settings.ANON_SAFE_SEARCH
         
-    # For any other kind of request...
-    else:
-        return render(request, 'dictionary/search_result.html')    
+        if request.user.has_perm('dictionary.search_gloss'):
+            # staff get to see all the words that have at least one translation
+            words = Keyword.objects.filter(text__istartswith=term, translation__isnull=False).distinct()
+        else:
+            # regular users see either everything that's published
+            # not sure what is going on here...
+            words = Keyword.objects.filter(text__istartswith=term,
+                                            translation__gloss__inWeb__exact=True).distinct()        
+        try:
+            crudetag = Tag.objects.get(name='lexis:crude')
+        except:
+            crudetag = None   
+        if safe and crudetag != None:    
+            crude = TaggedItem.objects.get_by_model(Gloss, crudetag)
+            # remove crude words from result 
+            
+            
+               
+    return render(request, 'dictionary/search_result.html')    
+    
+    
